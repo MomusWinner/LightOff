7a4b7bd02834aae97ea02970a8e7e8bd1d06146b
Areg <areg06.06@mail.ru>
2024-03-05 23:10:30 +0300

    implement Installers

---
+++ Assets/Scripts/Babaika/Intallers/BabaikaInstaller.cs
@@ -0,0 +1,15 @@
+﻿using Babaika.States;
+using Zenject;
+
+namespace Babaika
+{
+    public class BabaikaInstaller: MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            Container.Bind<BabaikaStateFactory>().AsSingle();
+            Container.BindFactory<IdleState, IdleState.Factory>().WhenInjectedInto<BabaikaStateFactory>();
+            Container.BindFactory<CatchingUpState, CatchingUpState.Factory>().WhenInjectedInto<BabaikaStateFactory>();
+        }
+    }
+}

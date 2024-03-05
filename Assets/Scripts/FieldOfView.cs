4d676a0d98581c79a5980b19b328c9a0a3c8889b
Areg <areg06.06@mail.ru>
2024-03-05 23:07:54 +0300

    refactored FieldOfView.cs

--- Assets/Scripts/FieldOfView.cs
+++ Assets/Scripts/FieldOfView.cs
@@ -1,52 +1,61 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 public class FieldOfView : MonoBehaviour
 {
-    public float viewRadius;
+    public Action<IReadOnlyList<Transform>> OnChangeVisibleTargets;
+    
+    public float ViewRadius;
     [Range(0,360)]
-    public float viewAngle;
+    public float ViewAngle;
 
-    public LayerMask targetMask;
-    public LayerMask obstacleMask;
+    [SerializeField] private LayerMask _targetMask;
+    [SerializeField] private LayerMask _obstacleMask;
 
-    [HideInInspector]
-    public List<Transform> visibleTargets = new ();
+    public IReadOnlyList<Transform> VisibleTargets => _visibleTargets;
 
-    void Start() {
-        StartCoroutine ("FindTargetsWithDelay", .2f);
-    }
+    private List<Transform> _visibleTargets = new();
+    
+    void Start() => StartCoroutine ("FindTargetsWithDelay", .2f);
 
-    IEnumerator FindTargetsWithDelay(float delay) {
-        while (true) {
+    IEnumerator FindTargetsWithDelay(float delay)
+    {
+        while (true)
+        {
             yield return new WaitForSeconds (delay);
             FindVisibleTargets ();
         }
     }
 
-    void FindVisibleTargets() {
-        visibleTargets.Clear ();
-        Collider[] targetsInViewRadius = Physics.OverlapSphere (transform.position, viewRadius, targetMask);
+    private void FindVisibleTargets() 
+    {
+        _visibleTargets.Clear ();
+        Collider[] targetsInViewRadius = Physics.OverlapSphere (transform.position, ViewRadius, _targetMask);
 
-        for (int i = 0; i < targetsInViewRadius.Length; i++) {
+        for (int i = 0; i < targetsInViewRadius.Length; i++) 
+        {
             Transform target = targetsInViewRadius [i].transform;
             Vector3 dirToTarget = (target.position - transform.position).normalized;
-            if (Vector3.Angle (transform.forward, dirToTarget) < viewAngle / 2) {
+            if (Vector3.Angle (transform.forward, dirToTarget) < ViewAngle / 2) 
+            {
                 float dstToTarget = Vector3.Distance (transform.position, target.position);
-
-                if (!Physics.Raycast (transform.position, dirToTarget, dstToTarget, obstacleMask)) {
-                    visibleTargets.Add (target);
-                }
+                
+                if (!Physics.Raycast (transform.position, dirToTarget, dstToTarget, _obstacleMask)) 
+                    _visibleTargets.Add (target);
             }
         }
+        
+        if(_visibleTargets.Any())
+            OnChangeVisibleTargets.Invoke(_visibleTargets);
     }
 
-    public Vector3 DirFromAngle(float angleInDegrees, bool angleIsGlobal) {
-        if (!angleIsGlobal) {
+    public Vector3 DirFromAngle(float angleInDegrees, bool angleIsGlobal) 
+    {
+        if (!angleIsGlobal) 
             angleInDegrees += transform.eulerAngles.y;
-        }
         return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad),0,Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
     }
 }
